f1b12b09edf9310e869e741c9c8c51266e253b8d
Rart3x <ramjattankeny971@gmail.com>
2025-12-10 15:57:35 +0100

    feat: Augmentation class and its methods

---
+++ classes/Augmentation.py
@@ -0,0 +1,43 @@
+class Augmentation:
+    """
+    Augmentation class
+    """
+    def __init__(self):
+        """
+        Augmentation constructor
+        """
+        pass
+
+    def __del__(self):
+        """
+        Augmentation constructor
+        """
+        pass
+
+    def original(self):
+        """"""
+        pass
+
+    def rotation(self):
+        """"""
+        pass
+
+    def blur(self):
+        """"""
+        pass
+
+    def contrast(self):
+        """"""
+        pass
+
+    def scaling(self):
+        """"""
+        pass
+
+    def illumination(self):
+        """"""
+        pass
+
+    def projective(self):
+        """"""
+        pass
