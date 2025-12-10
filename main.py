2a9a30da9819ee68e39ccf7f61500c37a3284521
Rart3x <ramjattankeny971@gmail.com>
2025-12-10 15:59:17 +0100

    refact: flake8 normed

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ def main():
     Main function
     """
     if len(sys.argv) != 2:
-        print(f"{Fore.RED}Usage: python augmentation.py file_path{Style.RESET_ALL}")
+        print(f"{Fore.RED}"
+              f"Usage: python augmentation.py file_path"
+              f"{Style.RESET_ALL}")
         return
 
     if not os.path.isfile(sys.argv[1]):
-        print(f"{Fore.RED}Error: argument need to be a file path{Style.RESET_ALL}")
+        print(f"{Fore.RED}"
+              f"Error: argument need to be a file path"
+              f"{Style.RESET_ALL}")
         return
-
 
 
 if __name__ == '__main__':
