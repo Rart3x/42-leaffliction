423f0f3330a7dadb12f7179cf00300a3cfa1d40b
Rart3x <ramjattankeny971@gmail.com>
2025-12-10 14:32:28 +0100

    feat: args type check

--- augmentation.py
+++ augmentation.py
@@ -1,5 +1,6 @@
 from colorama import Fore, Style
 
+import os
 import sys
 
 
@@ -8,7 +9,12 @@ def main():
     Main function
     """
     if len(sys.argv) != 2:
-        print(f"{Fore.RED}Usage: python augmentation.py path{Style.RESET_ALL}")
+        print(f"{Fore.RED}Usage: python augmentation.py file_path{Style.RESET_ALL}")
+        return
+
+    if not os.path.isfile(sys.argv[1]):
+        print(f"{Fore.RED}Error: argument need to be a file path{Style.RESET_ALL}")
+        return
 
 
 if __name__ == '__main__':
